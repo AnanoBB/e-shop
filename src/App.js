e543a00f057c8c3e12a13c37a5f79fc1d31e1a7f
AnanoBB <ana.berishvili.1@btu.edu.ge>
2020-12-24 04:24:55 +0400

    layouts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
-import Header from './components/header';
+
 import Homepage from "./pages/homepage";
 import Registration from "./pages/Registration";
+import MainLayout from './layouts/MainLayouts'
 import  {Switch,Route} from 'react-router-dom' 
 import "./default.scss";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <div className="main">
-        <Switch>
-          <Route exact path="/" component = {Homepage}/>
-          <Route path="/registarion" component={Registration}/>
+      <Switch>
+          <Route exact path="/"  render={() =>(
+            <MainLayout>
+              <Homepage />
+            </MainLayout>
+          ) }/>
+          <Route path="/registarion" render={()=>(
+            <MainLayout>
+              <Registration />
+            </MainLayout>
+          )}/>
         </Switch>
-       
-      
-      
-      </div>
+
       
       
     </div>
