8689cf5c1aad648913d188b2b369c1954f05ef19
AnanoBB <ana.berishvili.1@btu.edu.ge>
2020-12-24 14:19:13 +0400

    firebase set up

---
+++ src/firebase/config.js
@@ -0,0 +1,6 @@
+export const firebaseConfig = {apiKey: "AIzaSyBAoLPrmw7KT-KcbgnrSwZ41SPWpR285wg",
+authDomain: "e-shop-ea6da.firebaseapp.com",
+projectId: "e-shop-ea6da",
+storageBucket: "e-shop-ea6da.appspot.com",
+messagingSenderId: "458847224596",
+appId: "1:458847224596:web:384da196f95fe7b954b801"}
---
+++ src/firebase/utils.js
@@ -0,0 +1,6 @@
+import {firebaseConfig} from './config';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+
