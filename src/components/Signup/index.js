55e8ff244774db8fd210aed7ecba1fd3338dad36
AnanoBB <ana.berishvili.1@btu.edu.ge>
2020-12-24 16:29:29 +0400

    reusuble form component

---
+++ src/components/Signup/index.js
@@ -0,0 +1,18 @@
+import React, { Component} from 'react';
+import './styles.scss';
+
+class Signup extends Component{
+    render(){
+        return(
+            <div className='signup'>
+                <div className='wrap'>
+                    <h2>SignUp</h2>
+                    <form></form>
+                </div>
+            </div>
+        )
+    }
+}
+
+
+export default Signup
