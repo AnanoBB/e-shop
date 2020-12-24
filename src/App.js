1636bc24aa7d3ae45cd62ca27f60d0153edc402e
AnanoBB <ana.berishvili.1@btu.edu.ge>
2020-12-24 15:35:27 +0400

    database configurations

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 
 import Homepage from "./pages/homepage";
-import {auth} from './firebase/utils'
+import {auth, handleUserProfile} from './firebase/utils'
 import Registration from "./pages/Registration";
 import MainLayout from './layouts/MainLayouts';
 import HomepageLayout from './layouts/HomePageLayouts';
+
 import Login from './pages/Login';
 import  {Switch,Route,Redirect} from 'react-router-dom' 
 import "./default.scss";
@@ -24,16 +25,26 @@ class App extends Component {
   authListener = null;
 
   componentDidMount(){
-    this.authListener = auth.onAuthStateChanged(userAuth => {
-      if(!userAuth){
-        this.setState({
-          ...initialState
-        });
+    this.authListener = auth.onAuthStateChanged( async userAuth => {
+      if(userAuth){
+        const userRef = await handleUserProfile(userAuth);
+        userRef.onSnapshot(snapshot =>
+          {
+            this.setState({
+              currentUser : {
+                id:snapshot.id,
+                ...snapshot.data()
+
+
+              }
+            })
+          });
       }
 
       this.setState({
-        currentUser:userAuth
-      })
+        ...initialState
+      });
+     
     })
   }
 
