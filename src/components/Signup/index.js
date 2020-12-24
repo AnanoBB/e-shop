54e93a126a29ba67826bbf43b801b3198cebb095
AnanoBB <ana.berishvili.1@btu.edu.ge>
2020-12-24 16:37:40 +0400

    sign up form setup

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -1,13 +1,51 @@
 import React, { Component} from 'react';
 import './styles.scss';
+import FormInput from './../Forms/FormInput';
+import Button from './../Forms/Button';
+
+
+const initialState  = {
+    displayName : '',
+    email: '',
+    password : '',
+    confirmPassword : ''
+}
 
 class Signup extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            ...initialState
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+
+    }
+
+    handleChange(e){
+        const {name,value} = e.target;
+        this.setState({
+            [name] : value
+        });
+    }
     render(){
+
+        const {displayName} = this.state;
         return(
             <div className='signup'>
                 <div className='wrap'>
                     <h2>SignUp</h2>
-                    <form></form>
+                    <form>
+                        <FormInput 
+                        type='text'
+                        name = 'displayName'
+                        value ={displayName}
+                        placeholder='fullname'
+                        onChange={this.handleChange}
+                        />
+                        
+                    </form>
                 </div>
             </div>
         )
